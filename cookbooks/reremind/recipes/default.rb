9de92f6dfc781f51593795c04b9170a6e570ba5e
Howard Yeh <hayeah@gmail.com>
2010-06-09 16:09:29 -0700

    reremind

---
+++ cookbooks/reremind/recipes/default.rb
@@ -0,0 +1,10 @@
+Gem.clear_paths # needed for Chef to find the gem...
+require 'mysql' # requires the mysql gem
+
+execute "create #{node[:railsapp][:db][:database]} database" do
+  command "/usr/bin/mysqladmin -u root -p#{node[:mysql][:server_root_password]} create #{node[:reremind][:db][:name]}"
+  not_if do
+    m = Mysql.new("localhost", "root", @node[:mysql][:server_root_password])
+    m.list_dbs.include?(@node[:reremind][:db][:name])
+  end
+end
