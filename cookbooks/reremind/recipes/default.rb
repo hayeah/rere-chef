08c5ad0fe3ed39c155266951f5369f4fd986b2af
Howard Yeh <hayeah@gmail.com>
2010-06-10 13:30:36 -0700

    fixes

--- cookbooks/reremind/recipes/default.rb
+++ cookbooks/reremind/recipes/default.rb
@@ -6,7 +6,7 @@ execute "create database #{node[:rere][:db][:name]}" do
   command "/usr/bin/mysqladmin -u root -p#{node[:mysql][:server_root_password]} create #{node[:rere][:db][:name]}"
   not_if do
     m = Mysql.new("localhost", "root", @node[:mysql][:server_root_password])
-    m.list_dbs.include?(@node[:reremind][:db][:name])
+    m.list_dbs.include?(@node[:rere][:db][:name])
   end
 end
 
