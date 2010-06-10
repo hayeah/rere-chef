faa2d9301d9178a595bd4493d2b268ef0a3ffef1
Howard Yeh <hayeah@gmail.com>
2010-06-10 13:27:08 -0700

    rere recipe

--- cookbooks/reremind/recipes/default.rb
+++ cookbooks/reremind/recipes/default.rb
@@ -1,20 +1,23 @@
 Gem.clear_paths # needed for Chef to find the gem...
 require 'mysql' # requires the mysql gem
 
-execute "create #{node[:reremind][:db][:database]} database" do
-  command "/usr/bin/mysqladmin -u root -p#{node[:mysql][:server_root_password]} create #{node[:reremind][:db][:name]}"
+# create database
+execute "create database #{node[:rere][:db][:name]}" do
+  command "/usr/bin/mysqladmin -u root -p#{node[:mysql][:server_root_password]} create #{node[:rere][:db][:name]}"
   not_if do
     m = Mysql.new("localhost", "root", @node[:mysql][:server_root_password])
     m.list_dbs.include?(@node[:reremind][:db][:name])
   end
 end
 
-deploy "/app/reremind" do
-  repository "git://github.com/hayeah/rere.git"
-  user "rere"
-  group "rere"
-  shallow_clone true
-  environment "RAILS_ENV" => "production"
-  migrate true
-  migration_command "rake db:migrate"
-end
+# create database user for application
+
+# deploy "/app/reremind" do
+#   repository "git://github.com/hayeah/rere.git"
+#   user "rere"
+#   group "rere"
+#   shallow_clone true
+#   environment "RAILS_ENV" => "production"
+#   migrate true
+#   migration_command "rake db:migrate"
+# end
